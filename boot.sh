e1f746e4a861469c72db72f5c51b8d69b0a36be6
Alexander Ryabyshkin <86356347+wugdance@users.noreply.github.com>
2026-01-23 16:31:17 +0300

    Add user detecting.

--- boot.sh
+++ boot.sh
@@ -5,6 +5,17 @@
 # "-o pipefail" - (o stands for option) fail on pipeline erros.
 set -euo pipefail
 
+if [[ $EUID -eq 0 ]]; then
+    # This script is running as root.
+    if [ -z "$SUDO_USER" ]; then
+        echo "This script has to be run via sudo, not under root."
+        exit 1
+    else
+        WSARCH_USER="$SUDO_USER"
+        WSARCH_USER_HOME=$(getent passwd "$WSARCH_USER" | cut -d: -f6)
+    fi
+fi
+
 # "${BASH_SOURCE[0]}" - contains a path to the run script.
 # "$(dirname ...)"    - get direcotry name.
 # $(cd ... && pwd)    - resolve relative paths.
@@ -19,8 +30,9 @@ pacman -Syu --noconfirm
 
 # Somehow should not break the script.
 # source setup/user.sh
-source setup/locale.sh
-source setup/wsl.sh
+
+# source setup/locale.sh
+# source setup/wsl.sh
 
 source init/neovim.sh
 
