aadf27b6117d4603116f6dad4427daeb9c23bd09
Alexander Ryabyshkin <sintes4ek@gmail.com>
2026-01-23 19:50:44 +0300

    Del duplicates.

--- init/git.sh
+++ init/git.sh
@@ -4,8 +4,6 @@ pacman -S --noconfirm --needed git git-delta
 
 ln -sf "${WSARCH_ROOT}/config/git/.gitconfig" "${WSARCH_USER_HOME}/.gitconfig"
 ln -sf "${WSARCH_ROOT}/config/delta" "${WSARCH_USER_HOME}/.config/delta"
-ln -sf "/home/wugdance/wsarch/config/git/.gitconfig" "/home/wugdance/.gitconfig"
-ln -s "/home/wugdance/wsarch/config/delta" "/home/wugdance/.config/delta"
 
 delta --generate-completion bash
 
