e0515ee618c494ac21cfa9fac4fbe6fb1c30845b
Alexander Ryabyshkin <86356347+wugdance@users.noreply.github.com>
2026-01-23 15:51:26 +0300

    Add arch distro installation commands.

--- boot.ps1
+++ boot.ps1
@@ -1,2 +1,8 @@
+# Get latest wsl release version.
+wsl --update
+
+# Install arch distro.
+wsl --install archlinux
+
 # Set the new default user after creating.
 wsl --manage archlinux --set-default-user username
