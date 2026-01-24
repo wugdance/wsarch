e5f63f03b75f8ed4c8046523503183d061c31cb2
Alexander Ryabyshkin <sintes4ek@gmail.com>
2026-01-24 11:14:10 +0300

    Fix calling systemd for root user.

--- init/openssh.sh
+++ init/openssh.sh
@@ -2,16 +2,12 @@ echo "Init openssh..."
 
 pacman -S --noconfirm openssh
 
-# Create directory for user runtime variables
-sudo mkdir -p /etc/profile.d
-
-# Create a file to define the SSH_AUTH_SOCK path for all users
-echo 'export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"' | sudo tee /etc/profile.d/ssh-agent.sh
-
 # Reload systemd user configuration
-systemctl --user daemon-reload
+sudo -u "${WSARCH_USER}" XDG_RUNTIME_DIR="/run/user/${WSARCH_USER_ID}" \
+    systemctl --user daemon-reload
 
 # Enable the service (starts automatically on login)
-systemctl --user enable --now ssh-agent.service
+sudo -u "${WSARCH_USER}" XDG_RUNTIME_DIR="/run/user/${WSARCH_USER_ID}" \
+    systemctl --user enable --now ssh-agent.service
 
 echo "openssh init has completed."
