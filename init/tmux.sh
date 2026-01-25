f8c210dfe7b7fefe386a1ed5d0143acbc3a974a8
Alexander Ryabyshkin <sintes4ek@gmail.com>
2026-01-25 21:44:49 +0300

    Make tmux config modular.

--- init/tmux.sh
+++ init/tmux.sh
@@ -2,11 +2,11 @@ echo "Init tmux..."
 
 pacman -S --noconfirm tmux
 
-ln -s "${WSARCH_ROOT}/config/tmux/.tmux.conf" "${WSARCH_USER_HOME}/.tmux.conf"
+ln -s "${WSARCH_ROOT}/config/tmux" "${WSARCH_USER_HOME}/.config/tmux"
 
 git clone https://github.com/tmux-plugins/tpm \
-    "${WSARCH_USER_HOME}/.tmux/plugins/tpm"
+    "${WSARCH_USER_HOME}/.config/tmux/plugins/tpm"
 
-"${WSARCH_USER_HOME}/.tmux/plugins/tpm/bin/install_plugins"
+"${WSARCH_USER_HOME}/.config/tmux/plugins/tpm/bin/install_plugins"
 
 echo "tmux init has completed."
