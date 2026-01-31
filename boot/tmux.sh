64d8d5f45487e79270d022393067fe553a9b2d5e
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2026-01-31 21:42:22 +0300

    Add mkdir in case of.

--- boot/tmux.sh
+++ boot/tmux.sh
@@ -4,6 +4,7 @@ pacman -S --noconfirm tmux
 
 ln -s "${WSARCH_ROOT}/config/tmux" "${WSARCH_USER_HOME}/.config/tmux"
 
+mkdir -p "${WSARCH_USER_HOME}/.config/tmux/plugins"
 git clone https://github.com/tmux-plugins/tpm \
 "${WSARCH_USER_HOME}/.config/tmux/plugins/tpm"
 
