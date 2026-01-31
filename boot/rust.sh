5ef280dd94c53a17a174db8fffb2c81fa572a0f7
Alexander Ryabyshkin <86356347+wugdance@users.noreply.github.com>
2026-01-31 19:00:44 +0300

    Fix rust boot.

--- boot/rust.sh
+++ boot/rust.sh
@@ -1,5 +1,6 @@
 echo "Init rust..."
 
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf | sudo -u ${WSARCH_USER} sh -s -- -y
+source "${WSARCH_USER_HOME}/.cargo/env"
 
 echo "rust init has completed."
