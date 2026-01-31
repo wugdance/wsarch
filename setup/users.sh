37396b4c7ef35bcd23a634b1116ff29faae1c8e1
Alexander Ryabyshkin <86356347+wugdance@users.noreply.github.com>
2026-01-31 11:19:19 +0300

    Update and rename user.sh to users.sh

--- setup/users.sh
+++ setup/users.sh
@@ -1,5 +1,8 @@
 echo "Setup a new user..."
 
+echo "Setup the root password..."
+passwd
+
 read -p "Enter your username: " WSARCH_USER
 
 # Add a user:
@@ -13,19 +16,20 @@ useradd -m -G wheel -s /bin/bash $WSARCH_USER
 # (It will prompt to insert password twice.)
 passwd $WSARCH_USER
 
+pacman -S --noconfirm --needed sudo
 # Allow the wheel group to execute commands via `sudo`.
 
 # etc/sudoers.d/wheel - directory for sudo configuration fragments
-mkdir -p /etc/sudoers.d
-touch /etc/sudoers.d/wheel
-sh -c 'echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel'
+# mkdir -p /etc/sudoers.d
+# touch /etc/sudoers.d/wheel
+echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
 
 # Make it readable for owner and group.
 chmod 440 /etc/sudoers.d/wheel
 
-pacman -S --noconfirm --needed sudo
-# Validate sudoers syntax.
-visudo -c
+echo "" >> /etc/wsl.conf
+echo "[user]" >> /etc/wsl.conf
+echo "default=${WSARCH_USER}" >> /etc/wsl.conf
 
 echo "${WSARCH_USER} setup has completed."
 unset WSARCH_USER
