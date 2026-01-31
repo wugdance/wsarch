596dc2568b1f05c3279b98d8b0fba9ef2c074984
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2026-01-31 19:53:29 +0300

    Fix cargo access.

--- boot/neovim.sh
+++ boot/neovim.sh
@@ -4,13 +4,13 @@ pacman -S --noconfirm --needed neovim ripgrep fd tree-sitter-cli unzip
 
 # Install language specific dependencies:
 # ty - python language server.
-curl -LsSf https://astral.sh/ty/install.sh | sh
+curl -LsSf https://astral.sh/ty/install.sh | sudo -u ${WSARCH_USER} sh
 
 # ruff - python linter and core formatter.
-curl -LsSf https://astral.sh/ruff/install.sh | sh
+curl -LsSf https://astral.sh/ruff/install.sh | sudo -u ${WSARCH_USER} sh
 
 # stylua - lua code formatter.
-sudo -u "${WSARCH_USER}" cargo install stylua
+cargo install stylua
 
 # lua-language-server - yes.
 pacman -S --noconfirm --needed lua-language-server
