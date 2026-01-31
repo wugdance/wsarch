5888c8ce97e77cac4b32b6eb73833affd62c93a6
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2026-01-31 21:21:09 +0300

    Fix tmux tpm cloning.

--- boot/tmux.sh
+++ boot/tmux.sh
@@ -4,7 +4,8 @@ pacman -S --noconfirm tmux
 
 ln -s "${WSARCH_ROOT}/config/tmux" "${WSARCH_USER_HOME}/.config/tmux"
 
-git clone https://github.com/tmux-plugins/tpm \
+mkdir -p "${WSARCH_USER_HOME}/.config/tmux/plugins" && \
+    git clone https://github.com/tmux-plugins/tpm \
     "${WSARCH_USER_HOME}/.config/tmux/plugins/tpm"
 
 "${WSARCH_USER_HOME}/.config/tmux/plugins/tpm/bin/install_plugins"
