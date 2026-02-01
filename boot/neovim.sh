d1653bc3b58964ab81eefcce408bd6663336da57
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2026-02-01 13:30:20 +0300

    Upd lsp install scripts.

--- boot/neovim.sh
+++ boot/neovim.sh
@@ -4,10 +4,12 @@ pacman -S --noconfirm --needed neovim ripgrep fd tree-sitter-cli unzip
 
 # Install language specific dependencies:
 # ty - python language server.
-curl -LsSf https://astral.sh/ty/install.sh | sudo -u ${WSARCH_USER} sh -s -- --no-modify-path
+curl -LsSf https://astral.sh/ty/install.sh \
+    | sudo -u ${WSARCH_USER} env TY_NO_MODIFY_PATH=1 sh
 
 # ruff - python linter and core formatter.
-curl -LsSf https://astral.sh/ruff/install.sh | sudo -u ${WSARCH_USER} sh -s -- --no-modify-path
+curl -LsSf https://astral.sh/ruff/install.sh \
+    | sudo -u ${WSARCH_USER} env RUFF_NO_MODIFY_PATH=1 sh
 
 # stylua - lua code formatter.
 sudo -u "${WSARCH_USER}" -i cargo install stylua
