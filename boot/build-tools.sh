d3360d4e7b8931c1c13ac7ee32dfd452e97f2855
Alexander Ryabyshkin <86356347+wugdance@users.noreply.github.com>
2026-02-23 22:38:08 +0300

    Fix paru installation commands in build-tools.sh

--- boot/build-tools.sh
+++ boot/build-tools.sh
@@ -1,7 +1,7 @@
 pacman -S --noconfirm base-devel
 
-git clone https://aur.archlinux.org/paru.git /tmp/paru
-sudo -u "${WSARCH_USER}" makepkg -si -p /tmp/paru/PKGBUILD --noconfirm
+sudo -u "${WSARCH_USER}" git clone https://aur.archlinux.org/paru.git /tmp/paru
+sudo -u "${WSARCH_USER}" bash -c "cd /tmp/paru && makepkg -si --noconfirm"
 rm -rf /tmp/paru
 
 pacman -Syu --noconfirm
