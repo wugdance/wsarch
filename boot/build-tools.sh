a42301e10e3386bcf8f554b6e664da927bebdffd
Alexander Ryabyshkin <86356347+wugdance@users.noreply.github.com>
2026-02-23 22:08:46 +0300

    Fix pointing to pkgbuild file.

--- boot/build-tools.sh
+++ boot/build-tools.sh
@@ -1,7 +1,7 @@
 pacman -S --noconfirm base-devel
 
 git clone https://aur.archlinux.org/paru.git /tmp/paru
-sudo -u "${WSARCH_USER}" makepkg -si -p /tmp/paru --noconfirm
+sudo -u "${WSARCH_USER}" makepkg -si -p /tmp/paru/PKGBUILD --noconfirm
 rm -rf /tmp/paru
 
 pacman -Syu --noconfirm
