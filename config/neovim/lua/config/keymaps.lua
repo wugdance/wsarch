e048f66117463a37af7a3b42dd5e2d064d4e50e2
Alexander Ryabyshkin <sintes4ek@gmail.com>
2026-03-01 10:15:12 +0300

    Add kyemap for buffer name copy.

--- config/neovim/lua/config/keymaps.lua
+++ config/neovim/lua/config/keymaps.lua
@@ -1,2 +1,12 @@
 -- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear highlighted search matches." })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", {
+    desc = "Clear highlighted search matches.",
+})
+
+vim.keymap.set("n", "<leader>bn", function()
+    local path = vim.fs.relpath(vim.loop.cwd(), vim.api.nvim_buf_get_name(0))
+    vim.fn.setreg("+", path)
+    vim.notify("Copied: " .. path, vim.log.levels.INFO)
+end, {
+    desc = "Get current buffer name.",
+})
