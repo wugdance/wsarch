bd186016f87d88b31ea7a21d530e10b3a41a18e1
Alexander Ryabyshkin <sintes4ek@gmail.com>
2026-03-05 21:32:45 +0300

    solve different encoding position problem

--- config/neovim/lsp/ruff.lua
+++ config/neovim/lsp/ruff.lua
@@ -5,4 +5,13 @@ return {
     settings = {
         lineLength = 79,
     },
+    capabilities = {
+        general = {
+            -- pyright uses utf-16, and ruff uses utf-8 by default.
+            -- To avoid 'multiple different client offset_encodings ...',
+            -- we tell ruff to use 'utf-16' only
+            -- https://github.com/astral-sh/ruff/issues/14483
+            positionEncodings = { "utf-16" },
+        },
+    },
 }
