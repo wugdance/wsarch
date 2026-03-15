69a139c86f736a74d92bde6572d808e09e1d3ff8
Alexander Ryabyshkin <sintes4ek@gmail.com>
2026-03-15 21:05:49 +0300

    move funcs

--- config/bash/funcs.sh
+++ config/bash/funcs.sh
@@ -1,3 +1,21 @@
-pea() {
+function pea {
     source "$(poetry env info --path)/bin/activate"
+}
+
+function pacman::install {
+    pacman -Slq \
+        | fzf --multi --preview 'pacman -Si {1}' \
+        | xargs -ro sudo pacman -S
+}
+
+function pacman::remove {
+    pacman -Qq \
+        | fzf --multi --preview 'pacman -Qi {1}' \
+        | xargs -ro sudo pacman -Rns
+}
+
+function pytest::run {
+    target=$1
+
+    poetry run pytest -s -vv $target
 }
