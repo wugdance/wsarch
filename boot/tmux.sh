6370f3fcc1e738846bae4976741421b430ff622b
Alexander Ryabyshkin <86356347+wugdance@users.noreply.github.com>
2026-03-22 19:40:42 +0300

    Force symlink creation for tmux config
    Update symlink command to force overwrite existing link.

--- boot/tmux.sh
+++ boot/tmux.sh
@@ -2,7 +2,7 @@ echo "Init tmux..."
 
 pacman -S --noconfirm tmux
 
-ln -s "${WSARCH_ROOT}/config/tmux" "${WSARCH_USER_HOME}/.config/tmux"
+ln -sf "${WSARCH_ROOT}/config/tmux" "${WSARCH_USER_HOME}/.config/tmux"
 
 mkdir -p "${WSARCH_ROOT}/config/tmux/plugins"
 git clone https://github.com/tmux-plugins/tpm \
