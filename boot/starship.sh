cc19da709ff31781d272225815e28b2bf61ef496
Alexander Ryabyshkin <86356347+wugdance@users.noreply.github.com>
2026-03-22 19:41:26 +0300

    Fix symbolic link command in starship installation

--- boot/starship.sh
+++ boot/starship.sh
@@ -2,7 +2,7 @@ echo "Init starship..."
 
 curl -sS https://starship.rs/install.sh | sudo -u ${WSARCH_USER} sh -s -- -y
 
-ln -s "$WSARCH_ROOT/config/starship/starship.toml" \
+ln -sf "$WSARCH_ROOT/config/starship/starship.toml" \
     "$WSARCH_USER_HOME/.config/starship.toml"
 
 echo "starship init has completed."
